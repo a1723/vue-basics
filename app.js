9cb8b0b3a81c6d6a169dc79f88da128e68eb8fbf
Aleksandr <Sashkakonowawka4@gmail.com>
2021-09-03 00:51:11 +0300

    methods, v-for, v-if

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ const App = {
         return {
             title: 'Список заметок',
             placeholderString: 'Введите название заметки',
-            inputValue: ''
+            inputValue: '',
+            notes: ['Заметка 1', 'Заметка 2']
         }
-    }
+    },
+    methods: {
+        inputChangeHandler(event) {
+            this.inputValue = event.target.value;
+        },
+        addNewNote() {
+            this.notes.push(this.inputValue)
+            this.inputValue = ''
+        },
+        deleteNote(idx) {
+            this.notes.splice(idx, 1)
+        }
+    },
 }
 
 const app = Vue.createApp(App)
